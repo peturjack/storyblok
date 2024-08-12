43dcf764348669a6fded7d31e4f659d227f631b7
Petur jack <peturwj@gmail.com>
2024-08-12 15:32:53 +0000

    feat: added key in service section

--- app/[lang]/components/Services.tsx
+++ app/[lang]/components/Services.tsx
@@ -21,7 +21,10 @@ const Services = ({ data }: serviceProps) => {
       <div className="py-4 px-4 mx-auto max-w-screen-xl">
         <div className="space-y-8 lg:grid lg:grid-cols-2 sm:gap-6 xl:gap-10 lg:space-y-0 place-items-center items-stretch">
           {service_cards.map((s) => (
-            <div className="flex flex-col p-6 lg:mx-1 sm:mx-auto max-w-lg text-center rounded-lg border shadow border-gray-600 xl:p-8 text-white bg-neutral-800">
+            <div
+              key={s.title}
+              className="flex flex-col p-6 lg:mx-1 sm:mx-auto max-w-lg text-center rounded-lg border shadow border-gray-600 xl:p-8 text-white bg-neutral-800"
+            >
               <h3 className="mb-4 text-2xl font-semibold">{s.title}</h3>
               <p className="font-light text-xl text-gray-400">{s.subtitle}</p>
               <p className="text-xl mt-4">{s.description}</p>
